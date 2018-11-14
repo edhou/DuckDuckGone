c5bf07584eb0f92cafab830d2b7eb5d4d9c599b4
edhou <edhou@users.noreply.github.com>
2018-11-14 17:36:48 -0500

    Add title screen, combine aim and fire

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "random.h"
 #include "GLCD.h"
 #include "crosshair.h"
+#include "timer.h"
 
 // Yann Roberge - yroberge
 // Edward Hou - ehou
@@ -21,6 +22,7 @@ typedef struct {
 	uint16_t colours[3]; // 5-6-5 16-bit colours. Array size must be 3. Color 4 is transparency
 	uint32_t width,height;
 	unsigned char* pixels; // Each pixel has four possible colours, 1 byte per pixel
+	uint8_t* pixels; // Each pixel has four possible colours, 1 byte per pixel
 	int visible; // 0: hidden 1: shown
 } sprite_t;
 
@@ -38,56 +40,57 @@ uint32_t score;
 osMutexDef(newFrame); // waits for background to update sprite positions
 osMutexId(newFrameID);
 
+// Function to get joystick position
+unsigned char* joystickRead(void) {
+    static unsigned char* directions[] = {"left", "up", "right", "down", "none"};
+
+    int GPIOAStatus = LPC_GPIO1->FIOPIN;
+		if(!(GPIOAStatus & (1 << 23) ) )
+			return directions[0];
+		else if(!(GPIOAStatus & (1 << 24) ) )
+			return directions[1];
+		else if(!(GPIOAStatus & (1 << 25) ) )
+			return directions[2];
+		else if(!(GPIOAStatus & (1 << 26) ) )
+			return directions[3];
+		else
+			return directions[4];
+}
+
+
 // Define performance 
 const uint32_t TICKSPEED = 200; // 200 ticks per second
-const uint32_t SECOND = 1000; // 1000 ms in a second - needs 7000 to be close to a second
 const uint32_t FPS = 60;
 const uint32_t FRAMERATE = SECOND/FPS;
+const uint32_t delayLED = 5000;
+
 
 void monitor(void const *arg) {
 	
-	// Initalize LCD
-	GLCD_Init();
-	GLCD_Clear(BACKCOL);
-	GLCD_SetBackColor(BACKCOL);
-	GLCD_SetTextColor(0xE77D);
-	
-	GLCD_DisplayString(300, 1, 1, "Score: ");
-	GLCD_DisplayString(300, 10, 1, "Time: ");
+	GLCD_DisplayString(6, 1, 1, "Score: ");
+	GLCD_DisplayString(1, 13, 1, "Time: ");
 	
 	GLCD_WindowMax();
 	
-	//osMutexWait(newFrameID, osWaitForever);
-
+	osMutexWait(newFrameID, osWaitForever);
+	
 	// Render frames
-	unsigned int x1=60;
-	unsigned int y1=60;
-	unsigned int x2=120;
-	unsigned int y2=60;
-	unsigned int x3=150;
-	unsigned int y3=60;
-	unsigned int x4=60;
-	unsigned int y4=60;
 	while(1) {
-
+		
 		// Use mutex to pull array of sprites
 		
 		// Draw background
 		
 		// Draw sprites
-		//GLCD_Fill((100+x)%edge,100,60,60,BACKCOL);
-		GLCD_Bitmap_Move1px(&x1,&y1,60,60,crosshair_map, Down);
-		//GLCD_Bitmap_Move1px(&x2,&y2,60,60,crosshair_map, Right);
-		//GLCD_Bitmap_Move1px(&x3,&y3,60,60,crosshair_map, Up);
-		//GLCD_Bitmap_Move1px(&x4,&y4,60,60,crosshair_map, Down);
+		
 		// Wait until the next frame
 		osDelay(FRAMERATE);
 	}
 }
 
 void background(void const* arg) {
-	//newFrameID = osMutexCreate(osMutex(newFrame));
-	//osMutexWait(newFrameID, osWaitForever);
+	newFrameID = osMutexCreate(osMutex(newFrame));
+	osMutexWait(newFrameID, osWaitForever);
 	
 	// Create crosshair
 	crosshair.x = WIDTH/2;
@@ -95,17 +98,16 @@ void background(void const* arg) {
 	crosshair.colours[0] = Black;
 	crosshair.colours[1] = Black;
 	crosshair.colours[2] = Black;
-	crosshair.width = 60;
-	crosshair.height = 60;
+	crosshair.width = 70;
+	crosshair.height = 70;
 	crosshair.visible = 1;
-	crosshair.pixels = crosshair_map;
+	crosshair.pixels = malloc(sizeof(uint8_t) * (crosshair.width*crosshair.height)/16 );
 	
 	// Create ducks
 	int i =0;
 	int duckW = 70;
 	int duckH = 70;
-	unsigned char* duckPixels = malloc(sizeof(char)*duckW*duckH/16);
-	
+	uint8_t* duckPixels = malloc(sizeof(uint8_t)*duckW*duckH/16);
 	
 	for(i=0; i<NDUCKS; i++) {
 		ducks[i].x = 0;
@@ -116,7 +118,7 @@ void background(void const* arg) {
 		ducks[i].width = duckW;
 		ducks[i].height = duckH;
 		ducks[i].visible = 0;
-		ducks[i].pixels = duckPixels; // all ducks share the same image data. This saves memory.
+		ducks[i].pixels = duckPixels; // all ducks share the same image data
 	}
 	
 	// Create dead duck
@@ -128,42 +130,150 @@ void background(void const* arg) {
 	deadDuck.width = 70;
 	deadDuck.height = 70;
 	deadDuck.visible = 0;
-	deadDuck.pixels = malloc(sizeof(char) * (ducks[i].width*ducks[i].height) );
+	deadDuck.pixels = malloc(sizeof(uint8_t) * (ducks[i].width*ducks[i].height) );
 	
-	//osMutexRelease(newFrameID);
-	
-	while(1) {
-		osDelay(1000);
-	}
-}
-
-void aim(void const* arg) {
+	osMutexRelease(newFrameID);
 	
 	while(1) {
 		
 	}
 }
 
-void fire(void const* arg) {
+void aim(void const* arg) {
 	
+	float previousTime = 0;
+	float time = timer_read()/1E6;
+	float deltaTime = time - previousTime;
+	previousTime = time;
+	//MovePlayer(player_speed * delta_time, joystick_read()); 
+
+	// Loop
 	while(1) {
-	
+	   //GLCD_DisplayString(5, 3, 1, joystickRead()); //debug
 	}
 }
 
+void fire(void const* arg) {
+	
+	// Configure pins as outputs
+	LPC_GPIO1->FIODIR |= ((1 << 28) | (1 << 29) | (1 << 31)); // bits 28, 29, 31 set as outputs
+	LPC_GPIO2->FIODIR |= 0x0000007C; // bits 2 to 6 set as outputs
+	
+	
+	uint8_t fireEnable = 1; //1 = player can fire; 0 = player can't fire
+	
+	
+	// Inital condition: All LEDs on
+	
+	LPC_GPIO1->FIOSET |= (1 << 28);
+	LPC_GPIO1->FIOSET |= (1 << 29);
+	LPC_GPIO1->FIOSET |= (1 << 31);
+
+	LPC_GPIO2->FIOSET |= (1 << 2);
+	LPC_GPIO2->FIOSET |= (1 << 3);
+	LPC_GPIO2->FIOSET |= (1 << 4);
+	LPC_GPIO2->FIOSET |= (1 << 5);
+	LPC_GPIO2->FIOSET |= (1 << 6);	
+	
+	while(1){	
+		
+		if(fireEnable == 1){
+			// wait for button press
+			
+			// if button not pressed, wait for it to be pressed
+			while (LPC_GPIO2->FIOPIN & (1 << 10));
+			// if button pressed, wait for it to be released
+			while (!(LPC_GPIO2->FIOPIN & (1 << 10)));
+				
+			// do stuff on release
+			
+			//shoot();
+			GLCD_DisplayString(4, 3, 1, "fire"); //debug
+			
+			fireEnable = 0;
+			
+			LPC_GPIO1->FIOCLR |= (1 << 28);
+			LPC_GPIO1->FIOCLR |= (1 << 29);
+			LPC_GPIO1->FIOCLR |= (1 << 31);
+
+			LPC_GPIO2->FIOCLR |= (1 << 2);
+			LPC_GPIO2->FIOCLR |= (1 << 3);
+			LPC_GPIO2->FIOCLR |= (1 << 4);
+			LPC_GPIO2->FIOCLR |= (1 << 5);
+			LPC_GPIO2->FIOCLR |= (1 << 6);
+			
+			// clear all LEDs	
+			LPC_GPIO1->FIOCLR |= (1 << 28);
+			LPC_GPIO1->FIOCLR |= (1 << 29);
+			LPC_GPIO1->FIOCLR |= (1 << 31);
+
+			LPC_GPIO2->FIOCLR |= (1 << 2);
+			LPC_GPIO2->FIOCLR |= (1 << 3);
+			LPC_GPIO2->FIOCLR |= (1 << 4);
+			LPC_GPIO2->FIOCLR |= (1 << 5);
+			LPC_GPIO2->FIOCLR |= (1 << 6);
+			
+			osDelay(delayLED);
+			GLCD_DisplayString(4, 3, 1, "    "); //debug
+			LPC_GPIO1->FIOSET |= (1 << 28);	
+			osDelay(delayLED);
+			LPC_GPIO1->FIOSET |= (1 << 29);
+			osDelay(delayLED);
+			LPC_GPIO1->FIOSET |= (1 << 31);
+			osDelay(delayLED);
+			LPC_GPIO2->FIOSET |= (1 << 2);
+			osDelay(delayLED);
+			LPC_GPIO2->FIOSET |= (1 << 3);
+			osDelay(delayLED);
+			LPC_GPIO2->FIOSET |= (1 << 4);
+			osDelay(delayLED);
+			LPC_GPIO2->FIOSET |= (1 << 5);
+			osDelay(delayLED);
+			LPC_GPIO2->FIOSET |= (1 << 6);
+		
+			fireEnable = 1;
+		}
+	}
+}
+
+
 int main(void) {
 	
+	// Initalize LCD
+	GLCD_Init();
+	GLCD_Clear(0xFE40);
+	GLCD_SetBackColor(0xFE40);
+	GLCD_SetTextColor(0x4A07);
+	
+	
+	GLCD_DisplayString(5, 2, 1, "DuckDuckGone");
+	GLCD_DisplayString(9, 1, 1, "INT0 to Begin");
+	
+	// if button not pressed, wait for it to be pressed
+	while (LPC_GPIO2->FIOPIN & (1 << 10));
+	// if button pressed, wait for it to be released
+	while (!(LPC_GPIO2->FIOPIN & (1 << 10)));
+	
+	GLCD_Clear(0x226E);
+	GLCD_SetBackColor(0x226E);
+	GLCD_SetTextColor(0xE77D);
+	
 	// Thread definitions
-	osThreadDef(monitor, osPriorityNormal, 1, 0); 
+	//osThreadDef(monitor, osPriorityNormal, 1, 0); 
 	//osThreadDef(background, osPriorityNormal, 1, 0); 
-	//osThreadDef(aim, osPriorityNormal, 1, 0);
-	//osThreadDef(fire, osPriorityNormal, 1, 0);
+	osThreadDef(aim, osPriorityNormal, 1, 0);
+	osThreadDef(fire, osPriorityNormal, 1, 0);
 	
 	// Initialize multi-threading
 	osKernelInitialize(); 
 	osKernelStart();
-	osThreadCreate(osThread(monitor), NULL);
+	
+	//osThreadCreate(osThread(monitor), NULL);
 	//osThreadCreate(osThread(background), NULL);
-	//osThreadCreate(osThread(aim), NULL);
-	//osThreadCreate(osThread(fire), NULL);
+	osThreadCreate(osThread(aim), NULL);
+	osThreadCreate(osThread(fire), NULL);
+	
+	
+	
 }
+
